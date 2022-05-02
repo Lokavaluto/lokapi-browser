7f4074d935da70916c4253382f8454f166bb4edb
Valentin Lab <valentin.lab@kalysto.org>
2022-05-02 10:26:15 +0200

    chg: switch to browser implementation
    The browser implementation uses ``fetch`` API and is more adapted
    and more compatible than previous one based on node interface.
    
    In particular, some incompatibility on iOS platform where found when
    loading package local files.
    
    Signed-off-by: Valentin Lab <valentin.lab@kalysto.org>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { LokAPIAbstract, e, t, RestExc } from '@lokavaluto/lokapi'
-import { httpRequest as nodeHttpRequest } from '@0k.io/node-request'
+import { httpRequest as nodeHttpRequest } from '@0k.io/browser-request'
 import { t as RequestTypes } from '@0k.io/types-request'
 
 
